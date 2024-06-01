81729a4af870d8c45b44c66251b091d46027d47f
RyuSw-cs <rsw1452@naver.com>
2024-06-02 01:02:27 +0900

    [FEAT] 이미지 리소스 추가

--- data/src/main/java/com/neodinary/reque/data/remote/dto/ServerResponse.kt
+++ data/src/main/java/com/neodinary/reque/data/remote/dto/ServerResponse.kt
@@ -1,6 +1,6 @@
 package com.neodinary.reque.data.remote.dto
 
-internal data class BaseResponse<T>(
+internal data class ServerResponse<T>(
     val code: Int,
     val message: String,
     val data: T
